d3a4a722f23876b3b6d7b0da667594cb6df91bb5
phamlong <phamlong123np@gmail.com>
2026-07-01 22:19:15 +0700

    chore: setup environment variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,30 +1,32 @@
-import { env } from '~/config/environment'
+import { env } from "~/config/environment";
 //Những domain được phép truy cập tới tài nguyên của Server
-export const WHITELIST_ORIGIN = [
-]
+export const WHITELIST_ORIGIN = ["https://fresh-food-web.vercel.app"];
 
-export const WEBSITE_DOMAIN = env.BUILD_MODE === 'production'
-	? env.WEBSITE_DOMAIN_PROD
-	: env.WEBSITE_DOMAIN_DEV
+export const WEBSITE_DOMAIN =
+  env.BUILD_MODE === "production"
+    ? env.WEBSITE_DOMAIN_PROD
+    : env.WEBSITE_DOMAIN_DEV;
 
-export const API_DOMAIN = env.BUILD_MODE === 'production'
-	? env.API_DOMAIN_PROD
-	: env.API_DOMAIN_DEV
+export const API_DOMAIN =
+  env.BUILD_MODE === "production" ? env.API_DOMAIN_PROD : env.API_DOMAIN_DEV;
 
-export const RECOMMENDATION_DOMAIN = env.BUILD_MODE === 'production'
-	? env.RECOMMENDATION_SERVICE_URL_PROD
-	: env.RECOMMENDATION_SERVICE_URL_DEV
+export const RECOMMENDATION_DOMAIN =
+  env.BUILD_MODE === "production"
+    ? env.RECOMMENDATION_SERVICE_URL_PROD
+    : env.RECOMMENDATION_SERVICE_URL_DEV;
 
-export const GHN_MASTER_DATA_API = 'https://dev-online-gateway.ghn.vn/shiip/public-api/master-data'
-export const GHN_SHIPPING_ORDER_API = 'https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order'
+export const GHN_MASTER_DATA_API =
+  "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data";
+export const GHN_SHIPPING_ORDER_API =
+  "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order";
 
 export const GHN_HEADERS = {
-  'Token': env.GHN_TOKEN,
-  'Content-Type': 'application/json'
-}
+  Token: env.GHN_TOKEN,
+  "Content-Type": "application/json",
+};
 
 export const GHN_SHOP_HEADERS = {
-  'Token': env.GHN_TOKEN,
-  'ShopId': env.GHN_SHOP_ID,
-  'Content-Type': 'application/json'
-}
+  Token: env.GHN_TOKEN,
+  ShopId: env.GHN_SHOP_ID,
+  "Content-Type": "application/json",
+};
